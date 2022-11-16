100cf7c763dd424756dfb951f66c9a9c9bea5cca
gerardo wemans <gerardo.wemans@gmail.com>
2022-11-16 16:54:34 +0000

    resolved exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "Destroying all restaurants.."
+Restaurant.destroy_all
+cat = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+puts "Creating restaurants.."
+20.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: cat[rand(0..4)]
+  )
+  puts "Restaurant #{restaurant.name}"
+end
+
+puts "Restaurants successfully created!"
